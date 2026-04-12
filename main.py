c9ff0454e36285a405f7188f64c0acc6691483ac
Kerby Charles <charleskerby16@gmail.com>
2026-04-11 23:21:27 -0300

    erro proposital

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 def read_root():
     return {"mensagem": "API funcionando!"}
 
-@app.get("/somar/{a}/{b}")
+@app.get("/soma/{a}/{b}")
 def somar(a: int, b: int):
     # Apenas retorna a soma dos dois números
     return {"resultado": a + b}
+
+@app.get("multiplicar/{a}/{b}")
+def multiplicar(a: int.b: int):
+    return{"resultado": a * b}
